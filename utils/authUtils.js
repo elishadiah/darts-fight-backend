63c1efaec177ea39f0a6db760df463b43dc675f5
elishadiah <elishadiah0528@gmail.com>
2025-03-29 02:01:28 +0700

    feat: generate avatars func

--- utils/authUtils.js
+++ utils/authUtils.js
@@ -23,10 +23,11 @@ const generateAvatars = () => {
   const avatars = [];
   const levels = 6;
   const avatarsPerLevel = 7;
-  const baseXP = 500;
-  const xpIncrement = 1000;
+  const baseXP = 100;
+  const baseXPIncrement = 300;
+  const levelMultiplier = 3;
   const baseScoring = 10;
-  const lvlScoringIncrement = 3;
+  const lvlScoringIncrement = 4;
   const scoringIncrement = 1;
   const baseCheckout = 0.1;
   const lvlCheckoutIncrement = 0.1;
@@ -37,31 +38,100 @@ const generateAvatars = () => {
   const baseBullseye = 0.1;
   const lvlBullseyeIncrement = 0.1;
   const bullseyeIncrement = 0.05;
+  const excludedScoringValues = [179, 178, 176, 175, 173, 172, 169];
 
   for (let level = 1; level <= levels; level++) {
+    const levelXPIncrement = Math.pow(level, levelMultiplier) * baseXPIncrement;
+    const levelScoringBase =
+      baseScoring + Math.pow(level, 2) * lvlScoringIncrement; // Non-linear increase
+
     for (let i = 1; i <= avatarsPerLevel; i++) {
+      const subLevelXP =
+        baseXP + (level - 1) * levelXPIncrement + (i - 1) * baseXPIncrement;
+      let subLevelScoringIncrement =
+        levelScoringBase + (i - 1) * scoringIncrement; // Linear increase
+
+      while (excludedScoringValues.includes(subLevelScoringIncrement)) {
+        subLevelScoringIncrement++;
+      }
+
+      // Ensure no overlap between levels
+      if (level > 1) {
+        const prevLevelMaxScoring =
+          baseScoring +
+          Math.pow(level - 1, 2) * lvlScoringIncrement +
+          (avatarsPerLevel - 1) * scoringIncrement;
+        if (subLevelScoringIncrement <= prevLevelMaxScoring) {
+          subLevelScoringIncrement = prevLevelMaxScoring + 1;
+        }
+      }
+
       avatars.push({
         title: `Virtual Avatar ${level}-${i}`,
         ranks: level,
         subLvl: i,
-        xp: baseXP + (level - 1) * 8 * xpIncrement + (i - 1) * xpIncrement,
-        img: `RANKAVATAR${level}_${i}`,
-        scoring:
-          baseScoring +
-          (level - 1) * 8 * lvlScoringIncrement +
-          (i - 1) * scoringIncrement,
-        checkout:
-          baseCheckout +
-          (level - 1) * 8 * lvlCheckoutIncrement +
-          (i - 1) * checkoutIncrement,
-        edge:
-          baseEdge +
-          (level - 1) * 8 * lvlEdgeIncrement +
-          (i - 1) * edgeIncrement,
-        bullseye:
-          baseBullseye +
-          (level - 1) * 8 * lvlBullseyeIncrement +
-          (i - 1) * bullseyeIncrement,
+        xp: subLevelXP,
+        imgName: `RANKAVATAR${level}_${i}`,
+        isLocked: false,
+        isSelected: false,
+        skillset: [
+          {
+            name: "XP Boost",
+            value: (level - 1) * 8 + i,
+            description: "xpBoostDescription",
+            color: "bg-green-500",
+          },
+          {
+            name: "Bullseye",
+            value:
+              baseBullseye +
+              (level - 1) * 8 * lvlBullseyeIncrement +
+              (i - 1) * bullseyeIncrement,
+            initValue:
+              baseBullseye +
+              (level - 1) * 8 * lvlBullseyeIncrement +
+              (i - 1) * bullseyeIncrement,
+            requiredXp: 75,
+            description: "bullseyeDescription",
+            color: "bg-red-500",
+          },
+          {
+            name: "Scoring",
+            value: subLevelScoringIncrement,
+            initValue: subLevelScoringIncrement,
+            requiredXp: 75,
+            description: "scoringDescription",
+            color: "bg-blue-500",
+          },
+          {
+            name: "Checkout",
+            value:
+              baseCheckout +
+              (level - 1) * 8 * lvlCheckoutIncrement +
+              (i - 1) * checkoutIncrement,
+            initValue:
+              baseCheckout +
+              (level - 1) * 8 * lvlCheckoutIncrement +
+              (i - 1) * checkoutIncrement,
+            requiredXp: 75,
+            description: "checkoutDescription",
+            color: "bg-yellow-500",
+          },
+          {
+            name: "Edge",
+            value:
+              baseEdge +
+              (level - 1) * 8 * lvlEdgeIncrement +
+              (i - 1) * edgeIncrement,
+            initValue:
+              baseEdge +
+              (level - 1) * 8 * lvlEdgeIncrement +
+              (i - 1) * edgeIncrement,
+            requiredXp: 75,
+            description: "edgeDescription",
+            color: "bg-purple-500",
+          },
+        ],
       });
     }
   }
